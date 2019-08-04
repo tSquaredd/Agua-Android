b84d92582aad491b3e0d83c3137ab6e94e3da0e0
tylerturnbull <tsquarredapplications@gmail.com>
2019-08-04 15:42:20 -0400

    Testing account change

--- app/src/main/java/com/tsquaredapplications/agua/MainActivity.kt
+++ app/src/main/java/com/tsquaredapplications/agua/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNav?.setupWithNavController(findNavController(R.id.nav_host_fragment))
+        // test
     }
 
 }
